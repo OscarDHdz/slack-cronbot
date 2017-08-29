e15636d0999a2a91a9b3f3fa4723d7618952363b
Hernandez <o.a.hernandez@accenture.com>
2017-08-29 17:02:36 -0500

    Added more validations

--- index.js
+++ index.js
@@ -5,15 +5,31 @@ const axios = require('axios');
 
 // Env Variables
 const SLACK_URL = process.env.SLACK_URL;
-const SLACK_MESSAGE = process.env.SLACK_URL;
+const SLACK_MESSAGE = process.env.SLACK_MESSAGE;
 const SLACK_CHANNEL = process.env.SLACK_CHANNEL;
+const CRON_TIME = process.env.CRON_TIME;
 
 var ValidateParameters = () => {
   console.log(`[36m%s[0m`, `# Validatig parameters...`);
-  if ( process.env.SLACK_URL ) console.log(`  OK - Using Slack webhook: [35m${process.env.SLACK_URL}[0m`);
+  if ( process.env.SLACK_URL )  console.log(`  OK - Using Slack webhook: [35m${process.env.SLACK_URL}[0m`);
     else {
       console.log(`[31m%s[0m`, `  ERROR - No SLACK_URL provided`);
       throw `Missing SLACK_URL Environment variable`
+    }
+  if ( process.env.SLACK_MESSAGE ) console.log(`  OK - Sending message: [35m${process.env.SLACK_MESSAGE}[0m`);
+    else {
+      console.log(`[31m%s[0m`, `  ERROR - No SLACK_MESSAGE provided`);
+      throw `Missing SLACK_MESSAGE Environment variable`
+    }
+  if ( process.env.SLACK_CHANNEL ) console.log(`  OK - To channel: [35m${process.env.SLACK_CHANNEL}[0m`);
+    else {
+      console.log(`[31m%s[0m`, `  ERROR - No SLACK_CHANNEL provided`);
+      throw `Missing SLACK_CHANNEL Environment variable`
+    }
+  if ( process.env.CRON_TIME ) console.log(`  OK - At: [35m${process.env.CRON_TIME}[0m`);
+    else {
+      console.log(`[31m%s[0m`, `  ERROR - No CRON_TIME provided`);
+      throw `Missing CRON_TIME Environment variable`
     }
   console.log(`[36m%s[0m`, `# Validation completed.`);
 
